04fc9a17f7b795bfd1e056d00a9cb5a1ed24362a
Kanha Ransingh <prakash@909>
2024-01-14 13:30:33 +0530

    vshjd

--- src/components/Room/Room.jsx
+++ src/components/Room/Room.jsx
@@ -12,7 +12,7 @@ const Room = () => {
 
         const appID = 541670763;
         const serverSecret = "9f7d794b7309eb22e6856617093066d4";
-        const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomId,  Date.now().toString(),  "prakash Kumar");
+        const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomId,  Date.now().toString(),  "Hindu mandir");
     
         const zc = ZegoUIKitPrebuilt.create(kitToken)
 
